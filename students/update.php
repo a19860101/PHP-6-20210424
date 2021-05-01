f06238a8f0364a588461bb92eb708f7988b9369f
Zac <a19860101@gmail.com>
2021-05-01 14:56:43 +0800

    20210501

---
+++ students/update.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("conn.php");
+    // extract($_REQUEST);
+    // $sql = "UPDATE students 
+    //         SET 
+    //             name = '$name',
+    //             mail = '$mail',
+    //             gender= '$gender
+    //             "
