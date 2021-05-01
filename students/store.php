e9b10b2215c6a88d32d2177724f05815d40e97d2
Zac <a19860101@gmail.com>
2021-05-01 14:19:55 +0800

    20210501

--- students/store.php
+++ students/store.php
@@ -1,1 +1,10 @@
 <?php
+    require_once("conn.php");
+    extract($_REQUEST);
+
+    $skill = implode(",",$skill);
+    print_r($skill);
+    $sql = "INSERT INTO students(name,mail,gender,edu,skill,comment)VALUES('$name','$mail','$gender','$edu','$skill','$comment')";
+    mysqli_query($conn,$sql);
+
+    header("location:index.php");
