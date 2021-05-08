6c77dbf288c644c648d2a425fbcddd39c548d03a
Zac <a19860101@gmail.com>
2021-05-08 12:26:02 +0800

    20210508

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -14,9 +14,25 @@
     // echo $error;
     // echo "<br>";
     // echo $size;
+    switch($type){
+        case "image/jpeg":
+            $ext = ".jpg";
+            break;
+        case "image/png":
+            $ext = ".png";
+            break;
+        case "image/gif":
+            $ext = ".gif";
+            break;
+        default:
+            echo "<script>alert('請使用正確的圖檔')</script>";
+            header("refresh:0;url=index.php");
+            return;
+    }
+    $img_name = md5(time()).$ext;
     if(!is_dir("images")){
         mkdir("images");
     }
     if($error == 0){
-        move_uploaded_file($tmp_name,"images/".$name);
+        move_uploaded_file($tmp_name,"images/".$img_name);
     }
