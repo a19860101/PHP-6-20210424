7c46678d5cdaa480a6c830c38e2f85b29ab08c8d
Zac <a19860101@gmail.com>
2021-05-08 14:14:33 +0800

    20210508

--- filesystem/index.php
+++ filesystem/index.php
@@ -11,5 +11,8 @@
         <input type="file" name="img">
         <input type="submit" value="上傳">
     </form>
+    <div>
+        <a href="list.php">圖片列表</a>
+    </div>
 </body>
 </html>
--- filesystem/list.php
+++ filesystem/list.php
@@ -7,13 +7,27 @@
     <title>Document</title>
 </head>
 <body>
+    <div>
+        <a href="index.php">上傳圖片</a>
+    </div>
     <?php
         $imgs = glob('images/*');
         foreach($imgs as $img){
     ?>
         <div>
             <img src="<?php echo $img; ?>" width="200">
+            <form action="" method="post">
+                <input type="hidden" name="img" value="<?php echo $img;?>">
+                <input type="submit" value="刪除">
+            </form>
         </div>
     <?php } ?>
+    <?php
+        if(isset($_POST["img"])){
+            // echo $_POST["img"];
+            unlink($_POST["img"]);
+            header("location:list.php");
+        }
+    ?>
 </body>
 </html>
