6ae4db7b0799aab63a84276f27b3c805d18439fb
Zac <a19860101@gmail.com>
2021-05-08 12:13:00 +0800

    20210508

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -5,12 +5,18 @@
     // echo $_FILES["img"]["error"];
 
     extract($_FILES["img"]);
-    echo $name;
-    echo "<br>";
-    echo $type;
-    echo "<br>";
-    echo $tmp_name;
-    echo "<br>";
-    echo $error;
-    echo "<br>";
-    echo $size;
+    // echo $name;
+    // echo "<br>";
+    // echo $type;
+    // echo "<br>";
+    // echo $tmp_name;
+    // echo "<br>";
+    // echo $error;
+    // echo "<br>";
+    // echo $size;
+    if(!is_dir("images")){
+        mkdir("images");
+    }
+    if($error == 0){
+        move_uploaded_file($tmp_name,"images/".$name);
+    }
