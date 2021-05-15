7a61190afb5fb43f06fa034cf04252dd39aea214
Zac <a19860101@gmail.com>
2021-05-15 10:28:54 +0800

    20210515

--- blog/post/function.php
+++ blog/post/function.php
@@ -1,2 +1,26 @@
 <?php
-    
+    function showAllPost(){
+        $pdo = pdo();
+        $sql = "SELECT * FROM posts";
+        $stmt = $pdo->prepare($sql);
+        try {
+            $stmt->execute();
+            $students = $stmt->fetchAll();
+            return $students;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    function showPost(){}
+    function storePost($request){
+        $pdo = pdo();
+        extract($request);
+        $sql = "INSERT INTO posts(title,content,created_at,updated_at)VALUES(?,?,now(),now())";
+        $stmt = $pdo->prepare($sql);
+        
+        try {
+            $stmt->execute([$title,$content]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
