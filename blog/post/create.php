a8942761d2bb46eb8454f9703ef9d4482ee9a43a
Zac <a19860101@gmail.com>
2021-05-22 10:57:57 +0800

    20210522

--- blog/post/create.php
+++ blog/post/create.php
@@ -47,6 +47,39 @@
         height: '500px',
         plugins: 'image code link lists',
         toolbar: 'removeformat | image code link | styleselect bullist numlist | bold italic forecolor underline strikethrough | alignleft aligncenter alignright',
+        image_title: true,
+        /* enable automatic uploads of images represented by blob or data URIs*/
+        automatic_uploads: true,
+         
+        images_upload_url: 'postAcceptor.php',
+        file_picker_types: 'image',
+        /* and here's our custom image picker*/
+        file_picker_callback: function (cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+
+
+
+            input.onchange = function () {
+            var file = this.files[0];
+
+            var reader = new FileReader();
+            reader.onload = function () {
+
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                cb(blobInfo.blobUri(), { title: file.name });
+            };
+            reader.readAsDataURL(file);
+            };
+
+            input.click();
+        },    
     })
 </script>
 <?php include("../template/footer.php"); ?>
