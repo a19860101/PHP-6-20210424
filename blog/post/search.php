606ddab508f222621ee7378b54cf21908f3b98a0
Zac <a19860101@gmail.com>
2021-05-22 14:30:01 +0800

    20210522

---
+++ blog/post/search.php
@@ -0,0 +1,11 @@
+<?php include("../template/header.php"); ?>
+<?php include("../template/nav.php"); ?>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-xl-8 col-10">
+            <h2>搜尋結果</h2>
+            <hr>
+        </div>
+    </div>
+</div>
+<?php include("../template/footer.php"); ?>
