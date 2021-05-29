5caac594b62b68402f2068d463e4fe967b4ac171
Zac <a19860101@gmail.com>
2021-05-29 16:42:06 +0800

    20210529

--- oo/basic.php
+++ oo/basic.php
@@ -3,15 +3,28 @@
         public $brand;
         public $color = "white";
 
+        private $engine = "4.0 V8";
+        // 只可在類別內使用，不包含繼承的類別
+
+        protected $maxSpeed = 180;
+        // 只可在類別內使用，包含繼承的類別
+
         public function run(){
             // return 'Car Run';
             return $this->color;
         }
+        public function speed(){
+            // return $this->engine;
+            return $this->maxSpeed;
+        }
     }
 
+    $toyota = new Car;
+    // echo $toyota->speed;
     // $test = new Car;
     // $test->brand ="Toyota";
     // $run = $test->run('red');
+    echo $toyota->engine;
 
     // print_r($run);
     // print_r($test->color);
@@ -22,12 +35,14 @@
     class Truck extends Car {
         public $color = 'brown';
         public function payload(){
-            return 'hello payload';
+            return 'hello payload'. $this->maxSpeed;
         }
     }
     $test = new Truck;
     // echo $test->color;
-    echo $test->color;
-    echo $test->run();
-
-    echo $test->payload();
+    // echo $test->color;
+    // echo $test->run();
+    // echo $test->payload();
+    echo $test->engine;
+    // echo $test->payload();
+    // echo $test->maxSpeed;
