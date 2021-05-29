70915acbe8ff9789b868f69fbf7a843f5622ae6e
Zac <a19860101@gmail.com>
2021-05-29 12:02:27 +0800

    20210529

--- php-js/gallery/index.php
+++ php-js/gallery/index.php
@@ -5,6 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        label {
+            display: inline-block;
+            width: 200px;
+            height: 150px;
+        }
+
+        label img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: center;
+        }
+    </style>
 </head>
 <body>
     <form action="">
@@ -19,8 +33,13 @@
                 $galleries = glob("images/*.{jpeg,jpg,png,gif,webp,JPG}",GLOB_BRACE);
                 foreach($galleries as $g){
             ?>
-            <img src="<?php echo $g; ?>" width="200">
+            <label for="<?php echo $g; ?>">
+                <img src="<?php echo $g; ?>" width="200">
+                <input type="radio" name="img" id="<?php echo $g; ?>" value="<?php echo $g; ?>">
+            </label>
             <?php } ?>
+            <hr>
+            <a href="#">送出</a>
         </div>
     </div>
 </body>
