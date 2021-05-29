c4062389e9cf44c0accfb97abf1625adf10e7c93
Zac <a19860101@gmail.com>
2021-05-29 16:29:09 +0800

    20210529

--- oo/basic.php
+++ oo/basic.php
@@ -9,13 +9,25 @@
         }
     }
 
-    $test = new Car;
-    $test->brand ="Toyota";
-    $run = $test->run('red');
+    // $test = new Car;
+    // $test->brand ="Toyota";
+    // $run = $test->run('red');
 
-    print_r($run);
-    print_r($test->color);
+    // print_r($run);
+    // print_r($test->color);
 
-    $test2 = new Car;
-    $test2->brand = "Nissan";
+    // $test2 = new Car;
+    // $test2->brand = "Nissan";
     // print_r($test2);
+    class Truck extends Car {
+        public $color = 'brown';
+        public function payload(){
+            return 'hello payload';
+        }
+    }
+    $test = new Truck;
+    // echo $test->color;
+    echo $test->color;
+    echo $test->run();
+
+    echo $test->payload();
