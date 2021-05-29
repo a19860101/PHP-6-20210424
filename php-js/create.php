5c41dc34b5343ec844157fe2bd84995d186ef8ab
Zac <a19860101@gmail.com>
2021-05-29 10:31:56 +0800

    20210529

--- php-js/create.php
+++ php-js/create.php
@@ -56,9 +56,23 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
     <script>
         $(function(){
-            // $('form').submit();
-            $('#storeStudent').submit();
+            // $('#storeStudent').submit();
             // $('.storeStudent').click();
+            $('form').submit(function(){
+               $.ajax({
+                   url: 'store.php',
+                   type: 'post',
+                   data: $('form').serialize(),
+                   success(){
+                       console.log('success');
+                       location.href="index.php";
+                   },
+                   error(){
+                       console.log('error');
+                   }
+               })
+               return false;
+            });
         })
     </script>
 </div>
