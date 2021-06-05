ff38359ee20ffb1de73bc907e97489a0f02f0f83
Zac <a19860101@gmail.com>
2021-06-05 09:36:46 +0800

    20210605

---
+++ oo/basic-3.php
@@ -0,0 +1,12 @@
+<?php
+    class Test {
+        //建構子
+        function __construct (){
+            echo 'Contstruct';
+        }
+        function qwer(){
+            echo 'qwer';
+        }
+    }
+
+    $a = new Test;
